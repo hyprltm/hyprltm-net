ff23501ea8c5f6867d32300645a29da7bc1fbe8a
sniper1720 <sniper1720@linuxtechmore.com>
2026-05-26 18:04:53 +0100

    fix: use \n escape instead of raw newline in speedtest output

--- hyprltm-net.sh
+++ hyprltm-net.sh
@@ -1890,8 +1890,7 @@ run_speedtest() {
     local dl_mbps=$(awk -v rate="$dl_rate" 'BEGIN { printf "%.2f", rate / 125000 }')
     local ul_mbps=$(awk -v rate="$ul_rate" 'BEGIN { printf "%.2f", rate / 125000 }')
 
-    local formatted_result="$icon_download  $dl_mbps Mbps  (Download)
-$icon_upload  $ul_mbps Mbps  (Upload)"
+    local formatted_result="$icon_download  $dl_mbps Mbps  (Download)\n$icon_upload  $ul_mbps Mbps  (Upload)"
     display_info_message "$formatted_result" "$tr_speedtest_menu" "$icon_speedtest"
 }
 
